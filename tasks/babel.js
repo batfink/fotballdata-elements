0173029a6f651058728add42ba443efa191c8ef4
Victor Nystad <vnystad@gmail.com>
2015-09-21 12:02:29 +0200

    add rollup

--- tasks/babel.js
+++ tasks/babel.js
@@ -1,16 +1,16 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var sourcemaps = require("gulp-sourcemaps");
-var concat = require("gulp-concat");
+var rollup = require('gulp-rollup');
 
 
 gulp.task('babel', function () {
     return gulp.src(['elements/**/*.js'])
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
+        .pipe(rollup())
         .pipe(babel({
             modules: 'umd'
         }))
-        .pipe(concat("elements.js"))
-        .pipe(sourcemaps.write("."))
+        // .pipe(sourcemaps.write("."))
         .pipe(gulp.dest('dist'));
 });
